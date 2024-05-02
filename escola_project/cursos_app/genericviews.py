cb5a03c0808e7996fabbb80c6d3c793810fbae03
java_ada <leonardoptluz@hotmail.com>
2024-05-01 22:43:29 -0300

    Modificando views para genericviews

---
+++ escola_project/cursos_app/genericviews.py
@@ -0,0 +1,26 @@
+from rest_framework import generics
+from .models import Curso, Avaliacao
+from .serializers import CursoSerializer, AvaliacaoSerializer
+
+
+class CursosAPIView(generics.ListCreateAPIView):
+    queryset = Curso.objects.all()
+    serializer_class = CursoSerializer
+
+
+
+class CursoAPIView(generics.RetrieveDestroyAPIView):
+    queryset = Curso.objects.all()
+    serializer_class = CursoSerializer
+
+
+
+class AvaliacoesAPIView(generics.ListCreateAPIView):
+    queryset = Avaliacao.objects.all()
+    serializer_class = AvaliacaoSerializer
+
+
+    
+class AvaliacaoAPIView(generics.RetrieveDestroyAPIView):   
+    queryset = Avaliacao.objects.all()
+    serializer_class = AvaliacaoSerializer
