3934a3f40a0577a7ec040950dbb8b95b938f87a0
java_ada <leonardoptluz@hotmail.com>
2024-05-03 14:43:05 -0300

    Adicionando realationships

--- escola_project/cursos_app/serializers.py
+++ escola_project/cursos_app/serializers.py
@@ -24,16 +24,25 @@ class AvaliacaoSerializer(serializers.ModelSerializer):
         #fields = '__all__'
 
 class CursoSerializer(serializers.ModelSerializer):
+    
+    #NESTED RELATIONSHIP
+    #avaliacoes = AvaliacaoSerializer(many=True, read_only=True)
+
+    #HYPERLINKED RELATED FIELD
+    #avaliacoes = serializers.HyperlinkedRelatedField(many=True, read_only=True, view_name='avaliacao-detail')
+
+    #PRIMARY KEY RELATED FIELD
+    avaliacoes = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
 
     class Meta:
         model = Curso
-        
         fields = (
             'id',
             'titulo',
             'url',
             'criacao',
             'ativo',
+            'avaliacoes',
         )
 
         #fields = '__all__'
